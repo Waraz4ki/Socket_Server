63a0e4079c04ad7ae19d04f4ca0333765fd8b2d8
Moritz Balogh <W4r4z4ki@gmail.com>
2024-04-10 01:48:53 +0200

    Two Part Server Implementet

--- src/server.py
+++ src/server.py
@@ -15,18 +15,41 @@ logging.basicConfig(filename=f"{SERVER_NAME}.log", encoding='utf-8', level=loggi
 PORT = 60_000
 HOST = "127.0.0.1"
 
+"""Two Part Server
+On execution, main component will create seperate process which is supposed to act as the com port.
+Com port will recieve connections and such....
+
+Main part will handle certaint commands like conneciton kill or such
+Connections are Threaded
+
+__________                        ______________
+|        |                        |            |
+|  Main  |----------------------> |  Comm port | <-------------- Connection
+|        |                        |            |
+__________                        ______________
+   / \
+    |
+    |
+    |
+ Command
+    
+"""
+
+
+
 class Socketserver:
     __keep_alive = True
-    __safe = True
     
     def __init__(self, host=str, port=int):
         self.host = host
         self.port = port
         
-        self.server = socket.socket(family=socket.AF_INET, type=socket.SOCK_STREAM)
-        self.server.bind((host,port))
-        self.server.listen()
-    
+        with socket.socket(family=socket.AF_INET, type=socket.SOCK_STREAM) as self.server:
+            self.server.bind((host,port))
+            self.server.listen()
+            
+            mp.Process(target=self.serve_forever, name="Comm-port").start()
+
     def serve_forever(self):
         print(f"Server started awaiting connection...")
         logger.info(f"Server started at {self.host} awaiting connection on port {self.port}")
@@ -34,36 +57,29 @@ class Socketserver:
         while self.__keep_alive:
             self.conn, self.addr = self.server.accept()
             
-            p = mp.Process(target=self.serve_connection, name=f"{self.addr[0]}")
-            p.start()
-        
-    def serve_connection(self):
-        connection = True
-        
-        print(f"Connection to {self.addr} established succesfully")
-        logger.info(f"Connection to {self.addr} established succesfully")
-        while connection:
-            try:
-                pass
-            except ConnectionResetError:
-                logger.warning(f"Connection {self.addr} has unexpectadly disconnected")
-                break
-        logger.info(f"{self.addr} has disconnected")
+            t = (self.conn, self.addr)
             
+            con = threading.Thread(target=self.serve_connection, args=(self.conn, self.addr))
+            con.start()
+            
+    def serve_connection(self, conn, addr):
+        print(f"Connection to {addr} established succesfully")
+        logger.info(f"Connection to {addr} established succesfully")
+        while True:
+            try:
+                raw = conn.recv(12312)
+                print(f"{threading.currentThread().getName()}: {raw}")
+            except ConnectionResetError:
+                logger.warning(f"Connection {addr} has unexpectadly disconnected")
+                break            
             
     def stop_connection(self, ip):
         for process in mp.active_children():
             if process.name == ip:
                 process.close()
 
-
     def stop_all(self):
         pass
-    
-    
-    def shutdown_request(self, requester):
-        logger.info(f"{requester} sent a request to shut down server")
-        
         
     def shutdown(self):
         """Stop Main Server loop
@@ -75,4 +91,3 @@ class Socketserver:
 
 if __name__ == "__main__":
     Server = Socketserver(HOST, PORT)
-    Server.serve_forever()
